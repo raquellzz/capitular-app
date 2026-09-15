60dced7f1f4db083c150a7d7e38e1eecfe709206
raquellzz <raquelcostafreire24@gmail.com>
2026-09-15 20:39:41 -0300

    ci: configura action com ktlint, detekt e build do android

--- desktopApp/build.gradle.kts
+++ desktopApp/build.gradle.kts
@@ -4,6 +4,13 @@ plugins {
     alias(libs.plugins.kotlinJvm)
     alias(libs.plugins.composeMultiplatform)
     alias(libs.plugins.composeCompiler)
+    id("org.jlleitschuh.gradle.ktlint") version "12.1.2" apply false
+    id("io.gitlab.arturbosch.detekt") version "1.23.8" apply false
+}
+
+allprojects {
+    apply(plugin = "org.jlleitschuh.gradle.ktlint")
+    apply(plugin = "io.gitlab.arturbosch.detekt")
 }
 
 dependencies {
